9b614daeefc7058f14881849730840d427b0170e
Ana Rita Norinho <ritanorinho98@hotmail.com>
2018-05-16 10:23:15 +0100

    working on rotation

--- tp6/MyInterface.js
+++ tp6/MyInterface.js
@@ -83,6 +83,11 @@ class MyInterface extends CGFinterface {
 		this.activeKeys={};
 	};
 	processKeyDown(event) {
+		/*switch(event.keyCode){
+			case 83: // S
+				
+			case 87: // W
+		}*/
 	this.activeKeys[event.code]=true;
 	};
 	processKeyUp(event) {
